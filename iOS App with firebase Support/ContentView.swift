a439fdf7244de24fb2a89f87144f99932f5c1436
Jalal Hemidach <outawa@gmail.com>
2026-03-14 01:13:38 -0400

    Initial Commit

---
+++ iOS App with firebase Support/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS App with firebase Support
+//
+//  Created by Jalal Hemidach on 3/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS App with firebase Support/iOS_App_with_firebase_SupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_with_firebase_SupportApp.swift
+//  iOS App with firebase Support
+//
+//  Created by Jalal Hemidach on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_with_firebase_SupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
