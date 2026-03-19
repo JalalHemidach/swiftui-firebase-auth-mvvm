c838387e19ff7cbc49eff8688dbb05776cd4e9a5
Jalal Hemidach <outawa@gmail.com>
2026-03-18 22:49:01 -0400

    EmailAndPasswordValidationViewModel

--- iOS App with firebase Support/ViewModels/EmailAndPasswordValidationViewModel.swift
+++ iOS App with firebase Support/ViewModels/EmailAndPasswordValidationViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+@Observable
+class EmailAndPasswordValidationViewModel {
+    //MARK: Properties
+    var email = ""
+    var password = ""
+    var confirmPassword = ""
+    
+    //MARK: Email Validation
+    func isValidEmail() -> Bool {
+        let regex = try! Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
+        return email.wholeMatch(of: regex) != nil
+    }
+    
+    //MARK: Password Validation
+    func isValidPassword() -> Bool {
+        let regex = try! Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$")
+        return password.wholeMatch(of: regex) != nil
+    }
+    
+    //MARK: Email & Password Validation
+    func isValidEmailAndPassword() -> Bool {
+        return isValidEmail() && isValidPassword()
+    }
+}
