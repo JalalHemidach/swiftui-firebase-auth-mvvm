c04b2ffe4ebd257dd25c6fe0a030c3c076d23131
Jalal Hemidach <outawa@gmail.com>
2026-03-18 22:52:34 -0400

    SignUpViewModel

--- iOS App with firebase Support/ViewModels/SignUpViewModel.swift
+++ iOS App with firebase Support/ViewModels/SignUpViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+protocol SignUpProtocol {
+    func signUp(email: String, password: String, Completion: @escaping () -> Void)
+    var shouldDisplaySignUpScreen: Bool { get set }
+}
+@Observable
+class SignUpViewModel: SignUpProtocol {
+    //MARK: Properties
+    var emailAndPasswordViewModel: EmailAndPasswordViewModel
+    var firstName = ""
+    var lastName = ""
+    var confirmPassword = ""
+    var shouldDisplaySignUpScreen: Bool
+    
+    //MARK: init
+    init() {
+        emailAndPasswordViewModel = EmailAndPasswordViewModel(isSignUpScreen: true)
+        shouldDisplaySignUpScreen = false
+//        emailAndPasswordViewModel.signUpDelegate = self
+    }
+    
+    //MARK: SignUp
+    func signUp(email: String, password: String, Completion: @escaping () -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error signing up: \(error.localizedDescription)")
+                return
+            }
+            print("Signed up successfully")
+            Completion()
+        }
+    }
+}
