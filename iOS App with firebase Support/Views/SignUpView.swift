caeb9b2ada664e48d0fa3a9013a3ba3b0d5ae4b3
Jalal Hemidach <outawa@gmail.com>
2026-03-18 22:52:19 -0400

    SignUpView

--- iOS App with firebase Support/Views/SignUpView.swift
+++ iOS App with firebase Support/Views/SignUpView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct SignUpView: View {
+    //MARK: @State
+    @State private var signUpViewModel = SignUpViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            //MARK: First Name
+            HStack {
+                Label("First Name", systemImage: "person.text.rectangle")
+                    .foregroundColor(.primary)
+            }
+            TextField("First Name", text: $signUpViewModel.firstName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            //MARK: Last Name
+            Label("Last Name", systemImage: "person.text.rectangle.fill")
+            TextField("Last Name (Optional)", text: $signUpViewModel.lastName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            //MARK: Email & Password View`
+            EmailAndPasswordView(emailAndPasswordViewModel: signUpViewModel.emailAndPasswordViewModel)
+        }
     }
 }
 
